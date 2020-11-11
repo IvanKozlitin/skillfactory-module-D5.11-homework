19a53123182d6e46be9903602424a3df52d02466
KI-Shadow <ivankozlitin2012@gmail.com>
2020-11-11 11:41:05 +0300

    Site completed

--- site1/p_library/urls.py
+++ site1/p_library/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import AuthorEdit, AuthorList, author_create_many, books_authors_create_many, friends_list
+from .views import AuthorEdit, AuthorList, author_create_many, books_authors_create_many
 
 app_name = 'p_library'
 urlpatterns = [
@@ -8,5 +8,4 @@ urlpatterns = [
     path('authors', AuthorList.as_view(), name='author_list'),
     path('author/create_many', author_create_many, name='author_create_many'),
     path('author_book/create_many', books_authors_create_many, name='author_book_create_many'),
-    path('friends-list', friends_list, name='friends_list'),
 ]
